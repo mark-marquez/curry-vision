62412eaa0dcd13878cd6e58ab814d56649c46b11
Mark Marquez <marquez6275@gmail.com>
2025-08-28 11:25:29 -0700

    Completed implementation of a simple Box class for the bounding box data. Created a class instead of a simple struct because there may be additional behaviour functions that I want to add to this Box class later.

--- include/curryvision/types.hpp
+++ include/curryvision/types.hpp
@@ -1,17 +1,15 @@
 #pragma once
 
+
 class Point {
     public:
         Point();
-
         Point(int init_x, int init_y);
 
         int get_x() const;
-
         int get_y() const;
 
         void set_x(int new_x);
-
         void set_y(int new_y);
 
         float distance_to(const Point& point) const; 
@@ -21,23 +19,25 @@ class Point {
         int y;
 };
 
+
 class Box {
     public:
         Box();
-
         Box(Point top_left, Point top_right, Point bottom_left, Point bottom_right);
 
-        int get_x() const;
+        Point get_top_left() const;
+        Point get_top_right() const;
+        Point get_bottom_left() const;
+        Point get_bottom_right() const;
 
-        int get_y() const;
-
-        void set_x(int new_x);
-
-        void set_y(int new_y);
-
-        float distance_to(const Point& point) const; 
+        void set_top_left(Point point);
+        void set_top_right(Point point);
+        void set_bottom_left(Point point);
+        void set_bottom_right(Point point);
 
     private:
-        int x;
-        int y;
+        Point top_left;
+        Point top_right;
+        Point bottom_left;
+        Point bottom_right;
 };
