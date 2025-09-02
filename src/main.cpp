3a84ac677b61752cf7f05df3d26440a42bd98235
Mark Marquez <marquez6275@gmail.com>
2025-09-02 15:54:56 -0700

    Adding avg find_ball runtime metrics to measure baseline performance.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <curryvision/video_stream.hpp>
 #include <curryvision/ball_detector.hpp>
+#include <chrono>
+
 
 int main() {
-    VideoStream stream { 1 };       
+    constexpr int CAMERA_INDEX = 1;
+    constexpr int NUM_FRAMES = 100;
+
+    VideoStream stream { CAMERA_INDEX };       
     BallDetector detector {};      
 
     if (!stream.start()) {
@@ -11,12 +16,20 @@ int main() {
         return 1;
     }
 
-    stream.show(true);               
+    stream.show(true);             
+    using clock = std::chrono::steady_clock;  
+    using microseconds = std::chrono::microseconds;
+    long long running_sum = 0;
 
-    for (int i = 0; i < 1000; ++i) {
+    for (int i = 0; i < NUM_FRAMES; ++i) {
         Frame frame = stream.get_frame();
         if (frame.width == 0 || frame.height == 0) continue;
+
+        auto start = clock::now();
         Ball ball = detector.find_ball(frame);
+        auto end = clock::now();
+        running_sum += std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
         detector.draw_ball(frame, ball);
         stream.display(frame);
         
@@ -28,5 +41,10 @@ int main() {
 
     stream.show(false);             
     stream.stop();
+
+    double average = (running_sum / NUM_FRAMES);
+    std::cout << "\n=== Baseline ===\n"
+                << "Frames measured       : " << NUM_FRAMES << "\n"
+                << "Avg find_ball runtime : " << average << " ms per frame\n";
     return 0;
 }
